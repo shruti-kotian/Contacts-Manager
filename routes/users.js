4fcc59d25be8954efbd80f049419fab88a81fa8d
shruti kotian <shrutikotian@shrutis-MacBook-Air-2.local>
2019-10-14 14:17:26 -0400

    clear git cache

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,61 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 // @route POST api/users
 // @desc Register a user
 // @access Public
-router.post('/', (req, res) => res.send('Register a user'));
+router.post('/', [
+    check('name', 'Please enter name').not().isEmpty(),
+    check('email', 'Please enter valid email').isEmail(),
+    check('password', 'Please enter password with 6 or more characters').isLength({ min: 6 })
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email: email }); //find if email exists
+        if (user) {
+            return res.status(400).json({ msg: 'User already exists' });
+        }
+
+        user = new User({
+            name: name,
+            email: email,
+            password: password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        //create JSON Web token
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 360000
+        }, (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+        });
+    } catch (err) {
+        console.log(err.message);
+        res.send('Server error');
+    }
+});
 
 module.exports = router;
 
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 
-const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) =>
-    res.json({ msg: 'Welcome to the Contact keeper Application' }));
+//Connect Database
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
+const PORT = process.env.PORT || 5000;
 
 //Define Routes
 app.use('/api/users', require('./routes/users'));
